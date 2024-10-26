b60555a160edf7077baedb4cc7e1db9a4d1c4922
chu-andrew <chu.andrew.h@gmail.com>
2024-10-26 15:52:04 -0500

    testing page basic layout

--- app/(tabs)/testing.tsx
+++ app/(tabs)/testing.tsx
@@ -1,74 +1,128 @@
 import { StyleSheet, Pressable, Text, View } from "react-native";
 
-export default function TestSelection() {
-    const tests = {
-        speechTherapy: {
-            esp: [
-                { title: "Syllable Differentiation" },
-                { title: "Spondee Cards" },
-                { title: "Minimal Pairs" }
-            ],
-        }
-    };
+type Test = {
+  title: string;
+};
 
-    return (
-        <View style={styles.container}>
-            <View>
-                <Text style={styles.testingSelectionStyles}>
-                    Speech Therapy
-                </Text>
-                <View>
-                    <Text style={styles.ESPTestStyles}>ESP Test</Text>
-                    <View style={styles.buttonGroup}>
-                        {tests?.speechTherapy?.esp.map(
-                            test =>
-                                <Pressable style={styles.button} onPress={() => { }}>
-                                    <Text style={styles.buttonText}>{test.title}</Text>
-                                </Pressable>
-                        )}
-                    </View>
-                </View>
-            </View>
+type TestType = {
+  title: string;
+  tests: Test[];
+};
+
+export default function TestSelection() {
+  const tests = [
+    {
+      title: "Speech Therapy",
+      testTypes: [
+        {
+          title: "ESP Test",
+          tests: [
+            { title: "Syllable Differentiation" },
+            { title: "Spondee Cards" },
+            { title: "Minimal Pairs" },
+          ],
+        },
+        {
+          title: "LHM-10/Ling Card",
+          tests: [{ title: "Placeholder" }],
+        },
+      ],
+    },
+    {
+      title: "Audiology",
+      testTypes: [
+        {
+          title: "Speech Recognition Testing",
+          tests: [{ title: "Spondee Cards" }],
+        },
+        {
+          title: "Word Recognition Testing",
+          tests: [{ title: "WIPI" }, { title: "NU-CHIPS" }],
+        },
+        {
+          title: "LHM-10/Ling Card",
+          tests: [{ title: "Placeholder" }],
+        },
+      ],
+    },
+  ];
+
+  return (
+    <View style={styles.container}>
+      {tests.map((testCategory) => (
+        <View style={styles.testCategory}>
+          <Text style={styles.testCategoryTitle}>{testCategory.title}</Text>
+          <View style={styles.testTypeGroup}>
+            {testCategory.testTypes.map((testType, i) => (
+              <TestTypeSelection key={i} testType={testType} />
+            ))}
+          </View>
         </View>
-    );
+      ))}
+    </View>
+  );
+}
+
+function TestTypeSelection({ testType }: { testType: TestType }) {
+  const { title, tests } = testType;
+  return (
+    <View style={styles.testType}>
+      <Text style={styles.testType}>{title}</Text>
+      <View style={styles.buttonGroup}>
+        {tests.map((test: Test, i: number) => (
+          <Pressable
+            style={styles.button}
+            key={i}
+            onPress={() => console.log(test.title)}
+          >
+            <Text style={styles.buttonText}>{test.title}</Text>
+          </Pressable>
+        ))}
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        marginTop: 50,
-        backgroundColor: "aliceblue"
-    },
-    testingSelectionStyles: {
-        fontFamily: 'inter',
-        fontWeight: 'bold',
-        fontSize: 35,
-        marginLeft: 20,
-        marginTop: 15,
-    },
-    ESPTestStyles: {
-        fontFamily: 'inter',
-        fontSize: 20,
-        marginLeft: 20,
-        marginTop: 35,
-    },
-    button: {
-        alignItems: 'center',
-        alignSelf: "center",
-        borderRadius: 20,
-        padding: 15,
-        backgroundColor: 'grey',
-    },
-    buttonText: {
-        fontSize: 16,
-        lineHeight: 21,
-        fontWeight: 'bold',
-        letterSpacing: 0.25,
-        color: 'white',
-    },
-    buttonGroup: {
-      flexDirection: "row",
-      backgroundColor: "red",
-      alignContent: "center",
-    }
-}
-);
+  container: {
+    marginTop: 50,
+  },
+  testCategoryTitle: {
+    fontFamily: "inter",
+    fontWeight: "semibold",
+    fontSize: 30,
+    marginTop: 15,
+    marginLeft: 20,
+  },
+  testCategory: {
+    marginBottom: 30,
+  },
+  testTypeGroup: {
+    flexDirection: "row",
+    alignContent: "center",
+  },
+  testType: {
+    fontFamily: "inter",
+    fontSize: 20,
+    margin: 15,
+  },
+  button: {
+    alignItems: "center",
+    alignSelf: "center",
+    borderRadius: 20,
+    padding: 15,
+    margin: 5,
+    backgroundColor: "grey",
+  },
+  buttonText: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
+  },
+  buttonGroup: {
+    flexDirection: "row",
+    alignContent: "center",
+  },
+});
