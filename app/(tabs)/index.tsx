df55242c2f842a23278dfc806cf2217660d59e3f
Flipfloppm <franklin.s.dai@gmail.com>
2024-11-02 15:34:22 -0500

    styling/env example/package changes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,52 +1,54 @@
-import { Text, View, StyleSheet, Alert } from "react-native";
-import AuthScreen from '../authScreen'
-import {AuthButton} from '../authScreen'
-import 'react-native-url-polyfill/auto'
-import { useState, useEffect } from 'react'
-import { supabase } from '../../lib/supabase'
-import { Session } from '@supabase/supabase-js'
+import { Session } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import "react-native-url-polyfill/auto";
+import { supabase } from "../../lib/supabase";
+import AuthScreen, { AuthButton } from "../authScreen";
 export default function Index() {
-  const [session, setSession] = useState<Session | null>(null)
+  const [session, setSession] = useState<Session | null>(null);
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
+      setSession(session);
+    });
 
     supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
+      setSession(session);
+    });
+  }, []);
 
   const handleLogout = async () => {
     const { error } = await supabase.auth.signOut();
-    
+
     if (error) {
-        Alert.alert("Logout Error", error.message);
+      Alert.alert("Logout Error", error.message);
     } else {
-        console.log("User logged out successfully");
+      console.log("User logged out successfully");
     }
-};
+  };
 
   return (
     <View style={styles.containerStyle}>
-        {session && session.user ? (
-            <View>
-                <Text>
-                    Welcome, {session.user.user_metadata.first_name} {session.user.user_metadata.last_name}!
-                </Text>
-                <Text>Your User ID: {session.user.id}</Text>
-                <AuthButton label="Sign Out" onPress={handleLogout} />
-            </View>
-        ) : (
-            <AuthScreen />
-        )}
+      {session && session.user ? (
+        <View>
+          <Text>
+            Welcome, {session.user.user_metadata.first_name}{" "}
+            {session.user.user_metadata.last_name}!
+          </Text>
+          <Text>Your User ID: {session.user.id}</Text>
+          <AuthButton label="Sign Out" onPress={handleLogout} />
+        </View>
+      ) : (
+        <AuthScreen />
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   containerStyle: {
-    flex: 1, justifyContent: 'center', alignItems: 'center' 
-  }
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
