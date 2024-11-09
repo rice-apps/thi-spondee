a610ba2ced18e60813f04ee38cb9d8600c1b0921
CoocooFroggy <45371102+CoocooFroggy@users.noreply.github.com>
2024-11-09 14:15:27 -0600

    Fix AsyncStorage

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
 // import Config from "react-native-config";
 import { createClient } from "@supabase/supabase-js";
-import { AppState, AsyncStorage } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import { AppState } from "react-native";
 import "react-native-url-polyfill/auto";
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
