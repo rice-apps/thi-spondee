52474cd557239648f50c0a984461c9d8762448b7
Flipfloppm <franklin.s.dai@gmail.com>
2025-01-12 15:16:21 +0800

    added sample button to play sound

--- app/(tabs)/profiles.tsx
+++ app/(tabs)/profiles.tsx
@@ -1,8 +1,39 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Audio } from "expo-av";
+import { useEffect, useState } from "react";
+
 export default function Profiles() {
+  async function playSound() {
+    const { sound } = await Audio.Sound.createAsync(require("./sample.mp3"));
+    await sound.playAsync();
+  }
+
   return (
     <View>
       <Text style={{ color: "green", fontSize: 50 }}>Profiles Page</Text>
+      <TouchableOpacity style={styles.buttonStyle} onPress={playSound}>
+        <Text
+          style={{
+            color: "black",
+            fontSize: 20,
+            fontWeight: "normal",
+            textAlign: "center",
+          }}
+        >
+          Play Sound
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonStyle: {
+    width: 140,
+    height: 50,
+    padding: 8,
+    justifyContent: "center",
+    borderRadius: 12,
+    backgroundColor: "#D9D9D9",
+  },
+});
