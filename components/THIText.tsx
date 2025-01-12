b04c9414c17d77c464c37119a63ff36151335754
Flipfloppm <franklin.s.dai@gmail.com>
2025-01-12 17:04:15 +0800

    added custom text component so all text uses Inter font family

---
+++ components/THIText.tsx
@@ -0,0 +1,14 @@
+import { Text, TextStyle, StyleProp } from "react-native";
+
+interface THITextProps {
+  children: React.ReactNode;
+  style?: StyleProp<TextStyle>;
+}
+
+export const THIText = ({ children, style }: THITextProps) => {
+  return (
+    <Text style={[{ fontFamily: "Inter", fontSize: 20 }, style]}>
+      {children}
+    </Text>
+  );
+};
