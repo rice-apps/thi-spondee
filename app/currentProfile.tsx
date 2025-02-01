83d95602691bef9ca67e69e052f17d53969d66ba
adhiratippur <adhira23@gmail.com>
2025-02-01 14:22:52 -0600

    Begin working on global profile
    Co-authored-by: deinbojack <deinbojack@users.noreply.github.com>

---
+++ app/currentProfile.tsx
@@ -0,0 +1,27 @@
+export const userData = {
+    CURRENT_ID: "placeholder",
+    FIRST_NAME: "placeholder",
+    LAST_NAME: "placeholder",
+    USERNAME: "placeholder",
+    EMOJI: "placeholder",
+  };
+  
+  export const setCurrentID = (id) => {
+    userData.CURRENT_ID = id;
+  };
+  
+  export const setFirstName = (name) => {
+    userData.FIRST_NAME = name;
+  };
+  
+  export const setLastName = (name) => {
+    userData.LAST_NAME = name;
+  };
+  
+  export const setUsername = (username) => {
+    userData.USERNAME = username;
+  };
+  
+  export const setEmoji = (emoji) => {
+    userData.EMOJI = emoji;
+  };
