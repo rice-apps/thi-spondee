9ec05da52bfb4a72a76cd6f5bbf3e77d7cdfd783
Flipfloppm <franklin.s.dai@gmail.com>
2025-02-26 22:10:31 -0600

    styling changes

--- components/spondee/SessionControls.tsx
+++ components/spondee/SessionControls.tsx
@@ -1,9 +1,9 @@
 import Settings from "@/app/(tabs)/settings";
+import { Trial } from "@/app/(tabs)/tests/spondee";
 import { router } from "expo-router";
+import { Dispatch, SetStateAction, useState } from "react";
 import { StyleSheet, TouchableOpacity, View } from "react-native";
 import { THIText } from "../THIText";
-import React, {Dispatch, SetStateAction, useState} from "react";
-import {Trial} from "@/app/(tabs)/tests/spondee";
 
 type SessionControlProps = {
   totalTrials: number;
@@ -14,9 +14,9 @@ type SessionControlProps = {
 };
 
 export interface SessionData {
-    attempts: Trial[];
-    soundEnabled: boolean;
-    numCards: number;
+  attempts: Trial[];
+  soundEnabled: boolean;
+  numCards: number;
 }
 
 export function SessionControls({
@@ -24,33 +24,37 @@ export function SessionControls({
   numCorrect,
   numCards,
   setNumCards,
-  attempts
+  attempts,
 }: SessionControlProps) {
+  const [soundEnabled, setSoundEnabled] = useState(true);
 
-    const [soundEnabled, setSoundEnabled] = useState(true);
-
-    const sessionData: SessionData = {
-        attempts,
-        soundEnabled,
-        numCards,
-    }
+  const sessionData: SessionData = {
+    attempts,
+    soundEnabled,
+    numCards,
+  };
 
   return (
     <View style={styles.controlsContainer}>
       <TouchableOpacity
         onPress={() => {
-            console.log("End Session");
-            console.log(numCorrect, totalTrials, "End Session");
-            router.push({
-                pathname: "/(tabs)/inputSessionNotes",
-                params: { sessionData: JSON.stringify(sessionData) },
-            });
+          console.log("End Session");
+          console.log(numCorrect, totalTrials, "End Session");
+          router.push({
+            pathname: "/(tabs)/inputSessionNotes",
+            params: { sessionData: JSON.stringify(sessionData) },
+          });
         }}
         style={styles.button}
       >
         <THIText style={styles.buttonText}>End Session</THIText>
       </TouchableOpacity>
-      <Settings numCards={numCards} setNumCards={setNumCards} soundEnabled={soundEnabled} setSoundEnabled={setSoundEnabled} />
+      <Settings
+        numCards={numCards}
+        setNumCards={setNumCards}
+        soundEnabled={soundEnabled}
+        setSoundEnabled={setSoundEnabled}
+      />
     </View>
   );
 }
