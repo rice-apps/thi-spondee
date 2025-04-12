8fe705b0745c7a0a1c00d6e62cdfe56afbdab44e
Flipfloppm <franklin.s.dai@gmail.com>
2025-04-12 15:21:47 -0500

    added filter buttons to profile

--- app/(tabs)/profiles.tsx
+++ app/(tabs)/profiles.tsx
@@ -1,83 +1,79 @@
-import Card from "@/components/home/Card";
-import TopBar from "@/components/home/TopBar";
-import { THIText } from "@/components/THIText";
-import AntDesign from "@expo/vector-icons/AntDesign";
-import { StyleSheet, View, TouchableOpacity, Image} from "react-native";
-import { userData } from "../../app/currentProfile";
-import { router } from "expo-router";
-import React, { useState } from 'react';
-import { Picker } from '@react-native-picker/picker';
-import DropDownPicker from 'react-native-dropdown-picker';
 import DropDown from "@/components/profiles/DropDown";
-
-
+import { THIText } from "@/components/THIText";
+import { useState } from "react";
+import { Image, StyleSheet, TouchableOpacity, View } from "react-native";
+import { userData } from "../../app/currentProfile";
 
 export default function Profiles() {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState(null);
   const [items, setItems] = useState([
-    { label: 'Last 7 Days', value: 'Last 7 Days' },
-    { label: 'Last Month', value: 'Last Month' },
-    { label: 'Last 3 Months', value: 'Last 3 Months' },
+    { label: "Last 7 Days", value: "Last 7 Days" },
+    { label: "Last Month", value: "Last Month" },
+    { label: "Last 3 Months", value: "Last 3 Months" },
   ]);
   const [dropdownWidth, setDropdownWidth] = useState(100);
 
   const [open2, setOpen2] = useState(false);
   const [value2, setValue2] = useState(null);
   const [items2, setItems2] = useState([
-    { label: 'Spondee Cards', value: 'Spondee Cards' },
-    { label: 'LHM-10/Ling Cards', value: 'LHM-10/Ling Cards' },
-    { label: 'Syllable Differentiation', value: 'Syllable Differentiation' },
+    { label: "Spondee Cards", value: "Spondee Cards" },
+    { label: "LHM-10/Ling Cards", value: "LHM-10/Ling Cards" },
+    { label: "Syllable Differentiation", value: "Syllable Differentiation" },
   ]);
   const [dropdownWidth2, setDropdownWidth2] = useState(100);
 
   // Function to dynamically adjust width based on text size
-  const onTextLayout = (event: { nativeEvent: { layout: { width: any; }; }; }) => {
+  const onTextLayout = (event: { nativeEvent: { layout: { width: any } } }) => {
     const textWidth = event.nativeEvent.layout.width;
     setDropdownWidth(Math.min(Math.max(textWidth + 40, 100), 250)); // Min 100, Max 250
   };
 
-      return (
+  return (
     <View style={styles.container}>
       <View style={styles.container2}>
-      <View style={styles.imageContainer}>
-        <View style={styles.image}>
-          <THIText style={styles.emoji}>{userData.EMOJI}</THIText>
+        <View style={styles.imageContainer}>
+          <View style={styles.image}>
+            <THIText style={styles.emoji}>{userData.EMOJI}</THIText>
+          </View>
         </View>
-      </View>
-      <View style={styles.name}>
-        <THIText
+        <View style={styles.name}>
+          <THIText
+            style={{
+              color: "black",
+              fontSize: 28,
+              fontWeight: "600",
+              textAlign: "left",
+            }}
+          >
+            {userData.USERNAME}
+          </THIText>
+        </View>
+        <View
           style={{
-            color: "black",
-            fontSize: 28,
-            fontWeight: "600",
-            textAlign: "left",
+            flex: 1,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
           }}
         >
-          {userData.USERNAME}
-        </THIText>
-      </View>
-      <View
-        style={{
-          flex: 1,
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <View style={{ alignItems: "flex-end" }}>
-          <TouchableOpacity
-            style={styles.buttonStyle}
-            //onPress={() => 
-          >
-            <Image
-                  source={require("../../assets/images/more-button.png")}
-                  style={{ width: 23, justifyContent: "center", alignItems: "center" }}
-                />
-          </TouchableOpacity>
+          <View style={{ alignItems: "flex-end" }}>
+            <TouchableOpacity
+              style={styles.buttonStyle}
+              //onPress={() =>
+            >
+              <Image
+                source={require("../../assets/images/more-button.png")}
+                style={{
+                  width: 23,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              />
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
-    </View>
 
       <View>
         <THIText
@@ -91,53 +87,29 @@ export default function Profiles() {
         </THIText>
       </View>
 
-      <View style={{flexDirection: "row", marginLeft: 20}}>
-
-      <View style={[styles.dropDownButtonStyle, {width: dropdownWidth}]}>
-        {/* Hidden text component to measure width dynamically */}
-        {/*<THIText style={styles.hiddenText} onLayout={onTextLayout}> */}
-        <THIText style={styles.hiddenText} >
-          {value || "Date"}
-        </THIText>
-      <DropDownPicker
-        open={open}
-        value={value}
-        items={items}
-        setOpen={setOpen}
-        setValue={setValue}
-        setItems={setItems}
-        placeholder="Date"
-        style={[styles.dropdown, {width: dropdownWidth, shadowColor: "#E0F7FF" }]}
-        dropDownContainerStyle = {[styles.dropdownContainer, {width: dropdownWidth}]}
-      />
-    </View>
-
-    <View style={[styles.dropDownButtonStyle, {width: dropdownWidth}]}>
-        {/* Hidden text component to measure width dynamically */}
-        {/*<THIText style={styles.hiddenText} onLayout={onTextLayout}> */}
-        <THIText style={styles.hiddenText} >
-          {value || "Test type"}
-        </THIText>
-      <DropDownPicker
-        open={open2}
-        value={value2}
-        items={items2}
-        setOpen={setOpen2}
-        setValue={setValue2}
-        setItems={setItems2}
-        placeholder="Test type"
-        style={[styles.dropdown, {width: dropdownWidth, shadowColor: "#E0F7FF" }]}
-        dropDownContainerStyle = {[styles.dropdownContainer, {width: dropdownWidth}]}
-      />
-    </View>
-    <DropDown name="Date" values={["Last 7 Days", "Last Month", "Last 3 Months"]}  />
-      
+      <View style={{ flexDirection: "row", marginTop: 10, gap: 10 }}>
+        <DropDown
+          name="Date"
+          values={["Last 7 Days", "Last Month", "Last 3 Months"]}
+        />
+        <DropDown
+          name="Test Type"
+          values={[
+            "Spondee Cards",
+            "LHM-10/Ling Cards",
+            "Syllable Differentiation",
+            "Minimal Pairs",
+            "NU-CHIPS",
+            "WIPI",
+          ]}
+        />
+        <DropDown
+          name="Accuracy"
+          values={["< 90%", "< 70%", "< 50%", "< 20%"]}
+        />
       </View>
       {/* rectangle that contains all the cards*/}
-      <View style={styles.cardsContainer}>
-        
-      </View>
-      
+      <View style={styles.cardsContainer}></View>
     </View>
   );
 }
@@ -167,7 +139,7 @@ const styles = StyleSheet.create({
     paddingTop: 15,
     backgroundColor: "#F6F6F6",
     borderRadius: 10,
-    alignSelf: "flex-start"
+    alignSelf: "flex-start",
   },
   dropdown: {
     minWidth: 85, // Minimum width to prevent collapse
--- components/profiles/DropDown.tsx
+++ components/profiles/DropDown.tsx
@@ -1,62 +1,108 @@
-import { TouchableOpacity, View, StyleSheet, Image} from "react-native";
 import { THIText } from "@/components/THIText";
+import { useEffect, useState } from "react";
+import { Image, StyleSheet, TouchableOpacity, View } from "react-native";
 
+const DropDown = ({
+  name,
+  values,
+}: {
+  name: string;
+  values: Array<string>;
+}) => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [selectedFilter, setSelectedFilter] = useState(name);
+  const dropdownValues = ["Reset Filter", ...values];
 
-
-const DropDown = ({ name, values }: { name: string; values: Array<string> }) => {
-    return (
-        <View style = {styles.container}>
-            <View>
-                <TouchableOpacity style={styles.buttonStyle}>
-                    <THIText style = {styles.textstyle}>
-                        {name} 
-                    </THIText>
-                    <Image
-                        source={require("../../assets/images/keyboard_arrow_down.png")}
-                        style={{ width: 10.68, height: 6.02, justifyContent: "center", alignItems: "center" }}
-                    />
-                </TouchableOpacity>
-            </View>
-            <View style={styles.box}>
-                {values.map((value) => {
-                    return (
-                        <TouchableOpacity>
-
-                        </TouchableOpacity>
-                    );
-                })}
-            </View>
+  useEffect(() => {
+    // do something whenever x changes
+    if(selectedFilter === "Reset Filter") {
+        setSelectedFilter(name)
+    }
+  }, [selectedFilter])
+  return (
+    <View style={styles.container}>
+      <View>
+        <TouchableOpacity
+          style={[
+            styles.buttonStyle,
+            showDropdown && { borderColor: "#95D0E7" }, // Apply blue border when showDropdown is true
+          ]}
+          onPress={() => setShowDropdown(!showDropdown)}
+        >
+          <THIText style={styles.textstyle}>{selectedFilter}</THIText>
+          <Image
+            source={require("../../assets/images/keyboard_arrow_down.png")}
+            style={{
+              width: 10.68,
+              height: 6.02,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+      {showDropdown ? (
+        <View style={styles.box}>
+          {dropdownValues.map((value) => {
+            return (
+              <TouchableOpacity
+                onPress={() => {
+                  setSelectedFilter(value);
+                  setShowDropdown(!showDropdown);
+                }}
+              >
+                <THIText style={styles.textstyle}>{value}</THIText>
+              </TouchableOpacity>
+            );
+          })}
         </View>
-    );
+      ) : (
+        <></>
+      )}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        width: 118,
-        height: 200,
-        borderColor: "red",
-        borderRadius: 10
+  container: {
+    width: 200,
+    height: 200,
+    borderColor: "red",
+    borderRadius: 10,
+  },
+  buttonStyle: {
+    width: "100%",
+    height: 32,
+    paddingHorizontal: 15,
+    justifyContent: "space-between",
+    borderRadius: 10,
+    borderColor: "gray",
+    alignItems: "center",
+    borderWidth: 1,
+    flexDirection: "row",
+    gap: 10,
+  },
+  textstyle: {
+    fontSize: 15,
+  },
+  box: {
+    height: "auto",
+    marginTop: 10,
+    gap: 10,
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: "white",
+    // iOS shadow properties
+    shadowColor: "#95D0E74D",
+    shadowOffset: {
+      width: 0,
+      height: 4,
     },
-    buttonStyle: {
-        width: 118,
-        height: 32,
-        paddingHorizontal: 15,
-        justifyContent: "space-between",
-        borderRadius: 10,
-        borderColor: "gray",
-        alignItems: "center",
-        borderWidth: 1,
-        flexDirection: "row",
-        gap: 10
-    },
-    textstyle: {
-        fontSize: 15
-    },
-    box: {
-        height: "auto",
-    }
-    
-
-});   
+    shadowOpacity: 0.8,
+    shadowRadius: 3.84,
+    // Android shadow
+    elevation: 5,
+  },
+});
 
 export default DropDown;
